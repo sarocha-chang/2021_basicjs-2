ec413aab116860bea5487d526d0c8c3494d31ee7
sarocha-chang <sarocha_chang@cmu.ac.th>
2021-05-28 16:39:17 +0700

    Commit act 1
    Number 1

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 document.getElementById('ok').addEventListener('click',function(e){
     alert('ok is clicked')
+    console.log(e)
 }
 )
+document.getElementById('cancel').addEventListener('mousemove',function(c){
+    var x = c.clientX;
+    var y = c.clientY;
+    console.log('x:'+x+','+'y:'+y)
+})
